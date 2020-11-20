bd3a3b66161120163d825c9824da8a432bf05934
Tink, Zhang <yunfezhang@ebay.com>
2020-11-20 15:09:31 +0800

    Add Bottom Navigation

--- app/src/main/java/ink/iamt/waterkeeper/MainActivity.kt
+++ app/src/main/java/ink/iamt/waterkeeper/MainActivity.kt
@@ -2,10 +2,23 @@ package ink.iamt.waterkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
