98aaabfab4712380f5566236686408133dbf982e
Tink Zhang <ZhangYunfengZJU@gmail.com>
2020-11-21 16:29:12 +0800

    Add splash screen

--- app/src/main/java/ink/iamt/waterkeeper/MainActivity.kt
+++ app/src/main/java/ink/iamt/waterkeeper/MainActivity.kt
@@ -11,6 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_WaterKeeper)
         setContentView(R.layout.activity_main)
         setupNavigation()
     }
