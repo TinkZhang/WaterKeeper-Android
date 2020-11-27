e995a27fe8c142297abf12b540d014c90ae1451a
Tink, Zhang <yunfezhang@ebay.com>
2020-11-27 09:59:11 +0800

    Move the header out of the recycleView to simplify the adapter

--- app/src/main/java/ink/iamt/waterkeeper/ui/TodayWaterAdapter.kt
+++ app/src/main/java/ink/iamt/waterkeeper/ui/TodayWaterAdapter.kt
@@ -3,7 +3,6 @@ package ink.iamt.waterkeeper.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.HeaderViewListAdapter
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -11,10 +10,9 @@ import ink.iamt.waterkeeper.R
 import ink.iamt.waterkeeper.Record
 import kotlinx.android.synthetic.main.item_today_rv_item.view.*
 
-class TodayWaterAdapter() : RecyclerView.Adapter<TodayWaterAdapter.TodayWaterViewHolder>() {
+class TodayWaterAdapter : RecyclerView.Adapter<TodayWaterAdapter.TodayWaterViewHolder>() {
 
-    open inner class TodayWaterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-    inner class TodayWaterHeaderHolder(itemView: View) : TodayWaterViewHolder(itemView)
+    inner class TodayWaterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     val diffCallback = object : DiffUtil.ItemCallback<Record>() {
         override fun areItemsTheSame(oldItem: Record, newItem: Record): Boolean {
@@ -30,39 +28,22 @@ class TodayWaterAdapter() : RecyclerView.Adapter<TodayWaterAdapter.TodayWaterVie
 
     fun submitList(list: List<Record>) = differ.submitList(list)
 
-    override fun getItemViewType(position: Int) = when(position) {
-        0 -> HEADER
-        else -> CONTENT
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = when(viewType) {
-        HEADER -> TodayWaterHeaderHolder(
-                LayoutInflater
-                        .from(parent.context)
-                        .inflate(R.layout.item_today_rv_header, parent, false)
-        )
-        else -> TodayWaterViewHolder(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodayWaterViewHolder {
+        return TodayWaterViewHolder(
                 LayoutInflater
                         .from(parent.context)
                         .inflate(R.layout.item_today_rv_item, parent, false)
         )
     }
 
-    override fun getItemCount() = differ.currentList.size + 1
+    override fun getItemCount() = differ.currentList.size
 
     override fun onBindViewHolder(holder: TodayWaterViewHolder, position: Int) {
-        when (holder.itemViewType) {
-            CONTENT -> holder.itemView.apply {
-                val record = differ.currentList[position - 1]
-                tvIdInRv.text = position.toString()
-                tvTimeInRv.text = record.time?.hour.toString() + " : " + record.time?.minute.toString()
-                tvAmountInRv.text = record.amount.toString()
-            }
+        holder.itemView.apply {
+            val record = differ.currentList[position]
+            tvIdInRv.text = (position + 1).toString()
+            tvTimeInRv.text = record.time?.hour.toString() + " : " + record.time?.minute.toString()
+            tvAmountInRv.text = record.amount.toString()
         }
-    }
-
-    companion object {
-        val HEADER = 1
-        val CONTENT = 2
     }
 }
